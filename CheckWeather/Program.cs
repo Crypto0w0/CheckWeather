dee3982967665bf7972624a6dd29fb544bf1946b
Alex Untilov <untiloval80@gmail.com>
2024-04-05 04:27:01 +0200

    Initial commit

---
+++ CheckWeather/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class CheckWeather
+{
+    static async Task Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter the city: ");
+            string city = Console.ReadLine();
+
+            using (HttpClient client = new HttpClient())
+            {
+                string url = $"https://weather.com/weather/tenday/l/{city}";
+
+                HttpResponseMessage res = await client.GetAsync(url);
+                string rbody = await res.Content.ReadAsStringAsync();
+                Console.WriteLine("The weather: " + rbody);
+            }
+        }
+        catch (WebException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
